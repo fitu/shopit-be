2348121a1d56c49bf87396349115656e7e5d060a
Victorio Matteucci <victorio.matteucci@gmail.com>
2022-07-11 12:34:28 -0300

    [fix/string]: fix t function from request

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,8 +3,12 @@ import express from "express";
 declare global {
     namespace Express {
         interface Request {
+            // For authentication
             userId: string;
             email: string;
+
+            // For i18n
+            t: (message: string) => string
         }
     }
 }
